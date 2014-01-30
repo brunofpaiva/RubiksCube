027ea0f6f89d084e9d4302d7f62d1f8d42f3b6cf
Bruno Paiva <brunofpaiwa@gmail.com>
2014-01-30 14:06:20 -0300

    Little Changes

--- RubikProject/src/com/iaproject/Main.java
+++ RubikProject/src/com/iaproject/Main.java
@@ -9,13 +9,13 @@ public class Main {
 //		RubikCube cube = Utils.shuffleCube("U2 F U' L' U B L' R' B2 U' L2 U' B' L R2 D U' L' B' R' D F L2 F' B2");// OK 5 moves
 //		RubikCube cube = Utils.shuffleCube("F B U L' R' U' D L2 D' R L' U' D2 F' U' L2 D' L R B2 L D' B' L' D'");// OK 8 moves
 //		RubikCube cube = Utils.shuffleCube("L2 U2 L2 R2 B' L2 F L2 R2 D' L U R' D L' U' L2 D' U L' B2 U F' D U'");// OK 9 moves
-		RubikCube cube = Utils.shuffleCube("D2 F U D2 R L U' B' R2 L B L' B L R2 D2 B' U' F' U L' D2 B' R D'");// OK 8 moves
+//		RubikCube cube = Utils.shuffleCube("D2 F U D2 R L U' B' R2 L B L' B L R2 D2 B' U' F' U L' D2 B' R D'");// OK 8 moves
 //		RubikCube cube = Utils.shuffleCube("U2 D' F B' R' L U R2 B' D2 F' L U2 R2 L' D' B' D' U2 B' L' F D L' D'");// OK 9 moves
 //		RubikCube cube = Utils.shuffleCube("D F L' U' R B2 R2 U' R D' U' L F' U2 F' R' B D2 U' B' D U F2 R2 F2");// OK 8 moves
 //		RubikCube cube = Utils.shuffleCube("L2 R2 B R L2 F2 B' U' L' D2 U B L2 R U D2 L2 B2 D L2 D R' U F' R2");// INFINITO! // OK 5 moves - R' L' B' D R 
 //		RubikCube cube = Utils.shuffleCube("F' B2 L' U D' R' D2 U2 R F R L F B2 U2 L' B L' U F R2 D R' U' R2");// OK 6 moves
 //		RubikCube cube = Utils.shuffleCube("F' L R' F L U2 D' B' R U2 R2 B2 L' U2 F R2 L2 B L D R2 L' U2 R2 D");// OK 6 moves
-//		RubikCube cube = Utils.shuffleCube("");//
+		RubikCube cube = Utils.shuffleCube("R2 U F2 B2 R' D2 L' B L R' F' D U R' U B D2 B' F2 L' F2 R2 L2 F B");//
 				
 		System.out.println("======");
 		System.out.println(cube.toString());
