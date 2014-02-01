3fb838d2f7587d2b7c2acb27bea2c6d8738a7783
Bruno Paiva <brunofpaiwa@gmail.com>
2014-02-01 03:09:32 -0300

    Third and Second Pair

--- RubikProject/src/com/iaproject/F2LSolver.java
+++ RubikProject/src/com/iaproject/F2LSolver.java
@@ -10,18 +10,15 @@ public class F2LSolver {
 	private ArrayList<EdgePiece> edges;
 	
 	private final int FIRST_PAIR = 1; // Green and Red
-//	private final int SECOND_PAIR = 2; // Green and Orange
-//	private final int THIRD_PAIR = 3; // Orange and Blue
+	private final int SECOND_PAIR = 2; // Green and Orange
+	private final int THIRD_PAIR = 3; // Orange and Blue
 //	private final int FOURTH_PAIR = 4; // Blue and Red
 	
 	public F2LSolver(RubikCube cube, String solution) {
 		this.cube = cube;
 		this.solution = solution;
 		this.corners = new ArrayList<CornerPiece>();
-		fillCorners();
 		this.edges = new ArrayList<EdgePiece>();
-//		this.availableCorners = new ArrayList<CornerPlace>();
-//		fillAvailableCorners();
 	}
 
 	public String getSolution(){
@@ -31,16 +28,18 @@ public class F2LSolver {
 	public void solveFirstTwoLayers() {
 		int pairNumber = 1;
 		
-		while (pairNumber <= 1){
+		while (pairNumber <= 3){
 			
 			System.out.println("====== CUBE ======");
 			System.out.println(cube.toString());
 			System.out.println("====== ==== ======");
 			
 			solveCorner(pairNumber);
-
-//			System.out.println("pairNumber: " + count);
 			pairNumber++;
+			
+			System.out.println("====== CUBE ======");
+			System.out.println(cube.toString());
+			System.out.println("====== ==== ======");
 		}
 	}
 	
@@ -51,9 +50,26 @@ public class F2LSolver {
 		switch (curPair) {
 		
 			case FIRST_PAIR:
+				fillCorners();
 				corner = correctFirstCorner();
 				fillEdges();
 				edge = correctFirstEdge();
+				solveF2LPair(corner, edge);
+				break;
+				
+			case SECOND_PAIR:
+				fillCorners();
+				corner = correctSecondCorner();
+				fillEdges();
+				edge = correctSecondEdge();
+				solveF2LPair(corner, edge);
+				break;
+				
+			case THIRD_PAIR:
+				fillCorners();
+				corner = correctThirdCorner();
+				fillEdges();
+				edge = correctThirdEdge();
 				solveF2LPair(corner, edge);
 				break;
 				
@@ -62,7 +78,7 @@ public class F2LSolver {
 		}
 
 	}
-	
+
 	private CornerPiece correctFirstCorner(){
 		int i = 0;
 		for (CornerPiece corner : corners){
@@ -88,7 +104,120 @@ public class F2LSolver {
 				}else{
 					
 				}
-				System.out.println("Retornou o corner");
+				System.out.println("Retornou o corner1");
+				return corner;
+			}
+		}
+		return null;
+	}
+	
+	private CornerPiece correctSecondCorner() {
+		int i = 0;
+		for (CornerPiece corner : corners){
+			i++;
+			if (corner.isThatCorner("w", "o", "g")){
+				
+				System.out.println("\nPosition of Second Corner before correction: " + i);
+				if (corner.getType() == LevelKind.UP){
+					if (i == 5){
+						cube.up(cube.getGreenSide());
+						cube.up(cube.getGreenSide());
+						solution += "U U ";
+					}else if (i == 6){
+						cube.up(cube.getGreenSide());
+						solution += "U ";
+					}else if (i == 8){
+						cube.upInv(cube.getGreenSide());
+						solution += "U' ";
+					}
+					corner.setPosition(7);
+					System.out.println("\nPosition of Second Corner after correction: 7");
+					
+				}else{ //LevelKind.BOTTOM
+					if (i == 2){ // OK
+						corner.setPosition(2);
+						System.out.println("\nPosition of Second Corner after correction: 2");
+					}else if(i == 3){
+						cube.rightInv(cube.getGreenSide());
+						cube.upInv(cube.getGreenSide());
+						cube.right(cube.getGreenSide());
+						cube.up(cube.getGreenSide());
+						cube.up(cube.getGreenSide());
+						solution += "R' U' R U U ";
+						corner.setPosition(7);
+						
+						corner.setType(LevelKind.UP);
+						corner.setColorH(cube.getYellowSide().getMatrix()[2][2]);
+						corner.setColorL(cube.getGreenSide().getMatrix()[0][2]);
+						corner.setColorR(cube.getOrangeSide().getMatrix()[0][0]);
+						
+						System.out.println("\nPosition of Second Corner after correction: 7");
+					}else{ //i == 4
+						cube.left(cube.getGreenSide());
+						cube.up(cube.getGreenSide());
+						cube.leftInv(cube.getGreenSide());
+						cube.up(cube.getGreenSide());
+						solution += "L U L' U ";
+						corner.setPosition(7);
+						
+						corner.setType(LevelKind.UP);
+						corner.setColorH(cube.getYellowSide().getMatrix()[2][2]);
+						corner.setColorL(cube.getGreenSide().getMatrix()[0][2]);
+						corner.setColorR(cube.getOrangeSide().getMatrix()[0][0]);
+						
+						System.out.println("\nPosition of Second Corner after correction: 7");
+					}
+				}
+				System.out.println("Retornou o corner2");
+				return corner;
+			}
+		}
+		return null;
+	}
+	
+	private CornerPiece correctThirdCorner() {
+		int i = 0;
+		for (CornerPiece corner : corners){
+			i++;
+			if (corner.isThatCorner("w", "b", "o")){
+				
+				System.out.println("\nPosition of Third Corner before correction: " + i);
+				if (corner.getType() == LevelKind.UP){
+					if (i == 5){
+						cube.up(cube.getGreenSide());
+						solution += "U ";
+					}else if (i == 7){
+						cube.upInv(cube.getGreenSide());
+						solution += "U' ";
+					}else if (i == 8){
+						cube.up(cube.getGreenSide());
+						cube.up(cube.getGreenSide());
+						solution += "U U ";						
+					}
+					corner.setPosition(6);
+					System.out.println("\nPosition of Third Corner after correction: 6");
+					
+				}else{ //LevelKind.BOTTOM
+					if(i == 3){ // OK
+						corner.setPosition(3);
+						System.out.println("\nPosition of Third Corner after correction: 2");
+
+					}else{ //i == 4
+						cube.left(cube.getGreenSide());
+						cube.up(cube.getGreenSide());
+						cube.leftInv(cube.getGreenSide());
+						solution += "L U L' ";
+						corner.setPosition(6);
+						
+						corner.setType(LevelKind.UP);
+						corner.setColorH(cube.getYellowSide().getMatrix()[0][2]);
+						corner.setColorL(cube.getOrangeSide().getMatrix()[0][2]);
+						corner.setColorR(cube.getBlueSide().getMatrix()[0][0]);
+						
+						System.out.println("\nPosition of Third Corner after correction: 6");
+					}
+				}
+				System.out.println("Retornou o corner3");
 				return corner;
 			}
 		}
@@ -107,31 +236,118 @@ public class F2LSolver {
 						cube.up(cube.getGreenSide());
 						cube.rightInv(cube.getGreenSide());
 						cube.upInv(cube.getGreenSide());
-						solution += "R U R' U'";
-						System.out.println("\nPositions of First Edge before correction: 6");
+						solution += "R U R' U' ";
+						System.out.println("\nPositions of First Edge after correction: 6");
+						
 						edge.setPosition(6);
+						edge.setType(LevelKind.UP);
+						edge.setColor1(cube.getYellowSide().getMatrix()[1][2]);
+						edge.setColor2(cube.getOrangeSide().getMatrix()[0][1]);
+						
 					}else if(i == 3){
 						cube.rightInv(cube.getGreenSide());
 						cube.up(cube.getGreenSide());
 						cube.right(cube.getGreenSide());
 						cube.upInv(cube.getGreenSide());
-						solution += "R' U R U'";
-						System.out.println("\nPositions of First Edge before correction: 6");
+						solution += "R' U R U' ";
+						System.out.println("\nPositions of First Edge after correction: 6");
+						
 						edge.setPosition(6);
+						//TODO
+						
+						
 					}else{
 						cube.left(cube.getGreenSide());
 						cube.up(cube.getGreenSide());
 						cube.leftInv(cube.getGreenSide());
-						solution += "L U L'";
-						System.out.println("\nPositions of First Edge before correction: 5");
+						solution += "L U L' ";
+						System.out.println("\nPositions of First Edge after correction: 5");
+						
 						edge.setPosition(5);
+						//TODO
 					}
 				}else{
 					//LevelKind.UP
+					System.out.println("\nPositions of First Edge after correction: " + i);
 					edge.setPosition(i);					
 				}
 				
-				System.out.println("Retornou o edge");
+				System.out.println("Retornou o edge1\n");
+				return edge;
+			}
+		}
+		return null;
+	}
+	
+	private EdgePiece correctSecondEdge() {
+		int i = 0;
+		for (EdgePiece edge : edges){
+			i++;
+			if (edge.isThatEdge("g", "o")){
+				System.out.println("\nPositions of Second Edge before correction: " + i);
+				if (edge.getType() == LevelKind.BOTTOM && i != 2){
+					if (i == 1){// Already Done
+					}else if(i == 2){// Correct Place
+						System.out.println("\nPositions of Second Edge after correction: 2");
+						edge.setPosition(2);
+					}else if(i == 3){
+						cube.rightInv(cube.getGreenSide());
+						cube.upInv(cube.getGreenSide());
+						cube.right(cube.getGreenSide());
+						solution += "R' U' R ";
+						System.out.println("\nPositions of Second Edge after correction: 5");
+						edge.setPosition(5);
+					}else{ //(i == 4)
+						cube.left(cube.getGreenSide());
+						cube.upInv(cube.getGreenSide());
+						cube.leftInv(cube.getGreenSide());
+						cube.up(cube.getGreenSide());
+						solution += "L U' L' U ";
+						System.out.println("\nPositions of Second Edge after correction: 8");
+						edge.setPosition(8);
+					}
+				}else{
+					//LevelKind.UP
+					System.out.println("\nPositions of Second Edge after correction: " + i);
+					edge.setPosition(i);					
+				}
+				
+				System.out.println("Retornou o edge2\n");
+				return edge;
+			}
+		}
+		return null;
+	}
+	
+	private EdgePiece correctThirdEdge() {
+		int i = 0;
+		for (EdgePiece edge : edges){
+			i++;
+			if (edge.isThatEdge("o", "b")){
+				System.out.println("\nPositions of Third Edge before correction: " + i);
+				if (edge.getType() == LevelKind.BOTTOM && i != 3){
+					if (i == 1){// Already Done
+					}else if(i == 2){// Already Done
+					}else if(i == 3){// Already Done
+						System.out.println("\nPositions of Third Edge after correction: 3");
+						edge.setPosition(3);
+					}else{ //(i == 4)
+						cube.left(cube.getGreenSide());
+						cube.up(cube.getGreenSide());
+						cube.leftInv(cube.getGreenSide());
+						cube.upInv(cube.getGreenSide());
+						solution += "L U L' U' ";
+						System.out.println("\nPositions of Third Edge after correction: 8");
+						edge.setPosition(8);
+						edge.setType(LevelKind.UP);
+					}
+				}else{
+					//LevelKind.UP
+					System.out.println("\nPositions of Third Edge after correction: " + i);
+					edge.setPosition(i);					
+				}
+				
+				System.out.println("Retornou o edge3\n");
 				return edge;
 			}
 		}
@@ -140,7 +356,6 @@ public class F2LSolver {
 
 	private void fillCorners(/*int curPair*/){
 		corners.clear();
-		
 //TODO  corner1, corner2 e corner3 poderão ser dispensados a medida que se resolvem os pares (cada par)
 //		if (curPair == 1){
 		CornerPiece corner1 = new CornerPiece(cube.getWhiteSide().getMatrix()[0][0],
@@ -279,29 +494,55 @@ public class F2LSolver {
 	
 	private void solveF2LPair(CornerPiece corner, EdgePiece edge){
 		
-		System.out.println("Hello Felas!\ncornerPos: "+corner.getPosition()+"\nedgePos: "+edge.getPosition());
+		System.out.println("Finding The F2L Case!\ncornerPos: "+corner.getPosition()+"\nedgePos: "+edge.getPosition());
 		
-		
-		if ( (corner.getPosition() == 8 && edge.getPosition() == 7) &&
-				(corner.getColorH().equals(edge.getColor1())) ){
+		// CASE F2L 1
+		if ( (corner.getPosition() == 8 && edge.getPosition() == 7) && // Seeing the Position
+			 (corner.getColorH().equals(edge.getColor1())) ){		   // Comparing orientation the Orientation    
 			cube.up(cube.getGreenSide());
 			cube.front(cube.getGreenSide());
 			cube.upInv(cube.getGreenSide());
 			cube.frontInv(cube.getGreenSide());
-			solution += "U F U' F'";
+			solution += "U F U' F' ";
+		}
+		
+		
+		System.out.println("CornerH: " +corner.getColorH());
+		System.out.println("EdgeL: " + edge.getColor2());
+		
+		// CASE F2L 17
+		if ( (corner.getPosition() == 7 && edge.getPosition() == 6) &&
+			 (corner.getColorH().equals("w")) && (corner.getColorL().equals(edge.getColor2())) ){
+			cube.right(cube.getGreenSide());
+			cube.up(cube.getGreenSide());
+			cube.up(cube.getGreenSide());
+			cube.rightInv(cube.getGreenSide());
+			cube.upInv(cube.getGreenSide());
+			cube.right(cube.getGreenSide());
+			cube.up(cube.getGreenSide());
+			cube.rightInv(cube.getGreenSide());
+			solution += "R U U R' U' R U R' ";
+		}
+		
+		// CASE F2L 33		
+// TODO Tem algo errado nesse caso com relação ao edge		
+		if ( (corner.getPosition() == 6 && edge.getPosition() == 3) &&
+			 (corner.getColorH().equals(edge.getColor2())) ){
+			System.out.println("FUNFOU!");
+			
+			cube.upInv(cube.getGreenSide());
+			cube.back(cube.getGreenSide());
+			cube.upInv(cube.getGreenSide());
+			cube.backInv(cube.getGreenSide());
+			cube.up(cube.getGreenSide());
+			cube.up(cube.getGreenSide());
+			cube.back(cube.getGreenSide());
+			cube.upInv(cube.getGreenSide());
+			cube.backInv(cube.getGreenSide());
+			
+			solution += "U' B U' B' U U B U' B' ";
+			
 		}
 		
 	}	
-	
-/*	private void fillAvailableCorners() {
-		availableCorners.add(CornerPlace.ONE);
-		availableCorners.add(CornerPlace.TWO);
-		availableCorners.add(CornerPlace.THREE);
-		availableCorners.add(CornerPlace.FOUR);
-		availableCorners.add(CornerPlace.FIVE);
-		availableCorners.add(CornerPlace.SIX);
-		availableCorners.add(CornerPlace.SEVEN);
-		availableCorners.add(CornerPlace.EIGHT);
-	}*/
-
 }
